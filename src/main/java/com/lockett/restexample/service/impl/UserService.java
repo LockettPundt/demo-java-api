b1dd906e3bed13399aea77211477ac37879a18c1
Lockett Pundt <kalifornium@hotmail.com>
2024-01-21 13:11:18 -0500

    fix(Service): :bug: Add missing @service annotation

--- src/main/java/com/lockett/restexample/service/impl/UserService.java
+++ src/main/java/com/lockett/restexample/service/impl/UserService.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.lockett.restexample.controllers.UserController;
 import com.lockett.restexample.entities.User;
@@ -18,6 +19,7 @@ import com.lockett.restexample.service.UserServiceInterface;
 
 import jakarta.transaction.Transactional;
 
+@Service
 public class UserService implements UserServiceInterface {
 
   private static final Logger logger = LoggerFactory.getLogger(UserController.class);
